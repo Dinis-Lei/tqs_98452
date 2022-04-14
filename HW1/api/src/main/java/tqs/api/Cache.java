af6117231e9d297d47a0832a71de0298e6874345
Dinis-Lei <dinislei@ua.pt>
2022-04-14 16:51:49 +0100

    implemented Cache and unit tests

---
+++ HW1/api/src/main/java/tqs/api/Cache.java
@@ -0,0 +1,147 @@
+package tqs.api;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Cache {
+    
+    private final long timeToLive;
+    private final HashMap<String, CachedData> cacheMap;
+	private final int capacity;
+
+    
+    public Cache(long ttl, final long timerInterval, int maxItems) {
+		this.timeToLive = ttl * 1000;
+		this.capacity = maxItems;
+		cacheMap = new HashMap<>();
+ 
+		if (timeToLive > 0 && timerInterval > 0) {
+ 
+			Thread t = new Thread(new Runnable() {
+				public void run() {
+					while (true) {
+						try {
+							
+							// Thread: A thread is a thread of execution in a program.
+							// The Java Virtual Machine allows an application to have multiple threads of execution running concurrently.
+							Thread.sleep(timerInterval * 100);
+						} catch (InterruptedException ex) {
+							ex.printStackTrace();
+						}
+						cleanup();
+					}
+				}
+			});
+ 
+			// setDaemon(): Marks this thread as either a daemon thread or a user thread.
+			// The Java Virtual Machine exits when the only threads running are all daemon threads.
+			// This method must be invoked before the thread is started.
+			t.setDaemon(true);
+			
+			// start(): Causes this thread to begin execution; the Java Virtual Machine calls the run method of this thread.
+			// The result is that two threads are running concurrently:
+			// the current thread (which returns from the call to the start method) and the other thread (which executes its run method).
+			t.start();
+		}
+	}
+
+    public void put(String key, ArrayList<CovidData> value) {
+		synchronized (cacheMap) {
+			if (this.size() < capacity){
+				// put(): Puts a key-value mapping into this map.
+				cacheMap.put(key, new CachedData(value));
+			}
+			else{
+				Comparator<Map.Entry<String, CachedData>> comp1 =  (v1, v2) -> v1.getValue().hits - v2.getValue().hits;
+				Comparator<Map.Entry<String, CachedData>> comp2 =  
+					(v1, v2) -> v1.getValue().lastAccessed.intValue() - v2.getValue().lastAccessed.intValue();
+				List<Map.Entry<String, CachedData>> a =
+				cacheMap.entrySet().stream().sorted(comp1.thenComparing(comp2)).collect(Collectors.toList());
+				//a.forEach((v) -> {
+				//	System.out.println(v.getKey());
+				//	System.out.println(v.getValue().lastAccessed);
+				//});
+				//System.out.println(a.get(0));
+				cacheMap.remove(a.get(0).getKey());
+				cacheMap.put(key, new CachedData(value));
+
+			}
+			
+		}
+	}
+ 
+	public ArrayList<CovidData> get(String key) {
+		synchronized (cacheMap) {
+			CachedData c;
+			c = (CachedData) cacheMap.get(key);
+ 
+			if (c == null)
+				return null;
+			else {
+				c.lastAccessed = System.currentTimeMillis();
+				return c.getData();
+			}
+		}
+	}
+ 
+	public void remove(String key) {
+		synchronized (cacheMap) {
+			cacheMap.remove(key);
+		}
+	}
+ 
+	public int size() {
+		synchronized (cacheMap) {
+			return cacheMap.size();
+		}
+	}
+
+    public void cleanup() {
+		long now = System.currentTimeMillis();
+		ArrayList<String> deleteKey = new ArrayList<>();
+ 
+		synchronized (cacheMap) {
+			cacheMap.forEach(
+                (key, value) -> {
+                    if (now > (timeToLive + value.lastAccessed)){
+                        deleteKey.add(key);
+                    }
+                }
+            );
+		}
+ 
+		for (String key : deleteKey) {
+			synchronized (cacheMap) {
+				
+				// remove(): Removes the specified mapping from this map.
+				cacheMap.remove(key);
+			}
+ 
+			// yield(): A hint to the scheduler that the current thread is willing to
+			// yield its current use of a processor.
+			// The scheduler is free to ignore this hint.
+			Thread.yield();
+		}
+	}
+
+
+    protected class CachedData {
+
+        public Long lastAccessed = System.currentTimeMillis();
+		public ArrayList<CovidData> value;
+		public int hits = 0;
+ 
+		protected CachedData(ArrayList<CovidData> value) {
+			this.value = value;
+		}
+
+		public ArrayList<CovidData> getData(){
+			this.hits++;
+			return value;
+		}
+    }
+}
--- HW1/api/src/test/java/tqs/api/CacheTest.java
+++ HW1/api/src/test/java/tqs/api/CacheTest.java
@@ -3,7 +3,6 @@ package tqs.api;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -18,7 +17,7 @@ public class CacheTest {
 
     @BeforeEach
     void setup() throws ParseException{
-        cache = new Cache(10000, 1000, 5);
+        cache = new Cache(3, 1, 5);
 
         ArrayList<CovidData> test1 = new ArrayList<>(){
             {
@@ -51,7 +50,30 @@ public class CacheTest {
 
 
     @Test
-    void testCrunchifyCleanup() {
+    void testCleanup() throws ParseException, InterruptedException {
+        Thread.sleep(2000);
+
+        assertThat(cache.size()).isEqualTo(3);
+        ArrayList<CovidData> test = new ArrayList<>(){
+            {
+                add(new CovidData("france", "+5", 1234L, 100000L, "+2", 3000L, 5000L, "2022-04-13"));
+                add(new CovidData("france", "+5", 1234L, 100000L, "+2", 3000L, 5000L, "2022-04-12"));
+                add(new CovidData("france", "+5", 1234L, 100000L, "+2", 3000L, 5000L, "2022-04-11"));
+            }
+        };                
+        cache.put("france", test);
+        assertThat(cache.size()).isEqualTo(4);
+        Thread.sleep(1000);
+        assertThat(cache.size()).isEqualTo(1);
+        assertThat(cache.get("france"))
+            .hasSize(3)
+            .extracting(CovidData::getDay, CovidData::getCountry)
+            .contains(
+                tuple(new SimpleDateFormat("yyyy-mm-dd").parse("2022-04-13"), "france"),
+                tuple(new SimpleDateFormat("yyyy-mm-dd").parse("2022-04-12"), "france"),
+                tuple(new SimpleDateFormat("yyyy-mm-dd").parse("2022-04-11"), "france")
+            );
+        assertThat(cache.get("portugal")).isNull();
 
     }
 
@@ -73,17 +95,45 @@ public class CacheTest {
     }
 
     @Test
-    void testPut() {
+    void testPut() throws ParseException, InterruptedException {
 
+        ArrayList<CovidData> test = new ArrayList<>(){
+            {
+                add(new CovidData("france", "+5", 1234L, 100000L, "+2", 3000L, 5000L, "2022-04-13"));
+                add(new CovidData("france", "+5", 1234L, 100000L, "+2", 3000L, 5000L, "2022-04-12"));
+                add(new CovidData("france", "+5", 1234L, 100000L, "+2", 3000L, 5000L, "2022-04-11"));
+            }
+        };
+        
+        cache.put("france", test);
+
+        assertThat(cache.size()).isEqualTo(4);
+
+        assertThat(cache.get("france"))
+            .hasSize(3)
+            .extracting(CovidData::getDay, CovidData::getCountry)
+            .contains(
+                tuple(new SimpleDateFormat("yyyy-mm-dd").parse("2022-04-13"), "france"),
+                tuple(new SimpleDateFormat("yyyy-mm-dd").parse("2022-04-12"), "france"),
+                tuple(new SimpleDateFormat("yyyy-mm-dd").parse("2022-04-11"), "france")
+            );
+        cache.put("france2", test);
+        cache.get("portugal");
+        cache.get("spain");
+        cache.put("france3", test);
+
+        assertThat(cache.size()).isEqualTo(5);
+        assertThat(cache.get("usa")).isNull();
     }
 
     @Test
     void testRemove() {
 
+        cache.remove("usa");
+
+        assertThat(cache.size()).isEqualTo(2);
+        assertThat(cache.get("usa")).isNull();
+
     }
 
-    @Test
-    void testSize() {
-
-    }
 }
