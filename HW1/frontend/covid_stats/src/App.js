bbcf37084e9d6b6b0c93f1d1277745b2a4fc9e92
Dinis-Lei <dinislei@ua.pt>
2022-04-18 21:43:38 +0100

    finished frontend

--- HW1/frontend/covid_stats/src/App.js
+++ HW1/frontend/covid_stats/src/App.js
@@ -15,11 +15,13 @@ function App() {
   const [country, setCountry] = useState('');
 
   const [data, setData] = useState('');
+  const [nDays, setNDays] = useState(3);
 
 
   useEffect(() => {    
-    let resp = fetchData(country); // data is a promise object
+    let resp = fetchData(country + '?numberOfDays=' + nDays); // data is a promise object
     resp.then(function (result) {
+        console.log(result);
         setData(result);
     });
 
@@ -29,18 +31,26 @@ function App() {
   return (
     <div className="App">
       <h2>Covid Stats</h2>
-      <div className='col-4 mx-3'>
+      <Form className='col-6 mx-3'>
         <InputGroup className="mb-3">
-          <Button variant="outline-secondary" id="button-addon1" onClick={() => setFlg(!flg)}>
+          <Button itemID='search-btn' variant="outline-secondary" id="button-addon1" onClick={() => setFlg(!flg)}>
             Search
           </Button>
           <FormControl
+            itemID='country-inpt'
             placeholder='Ex: usa'
             //value={country}
-            onChange={(e) => {setCountry(e.target.value);  console.log(country)}}
+            onChange={(e) => {setCountry(e.target.value);}}
+          />
+          <input
+            itemID='nDays-inpt'
+            type="number"
+            placeholder='3'
+            className='mx-3 col-2'
+            onChange={(e) => {setNDays(e.target.value);}}
           />
         </InputGroup>
-      </div>
+      </Form>
 
       <Table striped bordered hover variant='dark'>
         <thead>
@@ -56,18 +66,18 @@ function App() {
         </thead>
         {data != '' &&
           <tbody>
-            <tr>
-              {console.log(data)}
-              <td>{data.day}</td>
-              <td>{data.newCases}</td>
-              <td>{data.totalCases}</td>
-              <td>{data.activeCases}</td>
-              <td>{data.newDeaths}</td>
-              <td>{data.totalDeaths}</td>
-              <td>{data.totalTests}</td>
-            </tr>
-            
-
+              {data.map( (value, i) => {
+                console.log(value);
+                return <tr>     
+                        <td itemID={'day' + i}>{value.day == null ? 'NA' : value.day}</td>
+                        <td itemID={'newCases' + i}>{value.newCases == null ? 'NA' : value.newCases}</td>
+                        <td itemID={'totalCases' + i}>{value.totalCases == null ? 'NA' : value.totalCases}</td>
+                        <td itemID={'activeCases' + i}>{value.activeCases == null ? 'NA' : value.activeCases}</td>
+                        <td itemID={'newDeaths' + i}>{value.newDeaths == null ? 'NA' : value.newDeaths}</td>
+                        <td itemID={'totalDeaths' + i}>{value.totalDeaths == null ? 'NA' : value.totalDeaths}</td>
+                        <td itemID={'totalTests' + i}>{value.totalTests == null ? 'NA' : value.totalTests}</td>
+                      </tr>
+              } )}                        
           </tbody>
         }
         
