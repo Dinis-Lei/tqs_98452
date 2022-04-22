fe9d41981d219c25290459be633df3da4938cfe0
dinis-lei <dinislei@ua.pt>
2022-04-22 01:26:32 +0100

    frontend tests

--- HW1/api/src/test/java/tqs/api/WebPageSteps.java
+++ HW1/api/src/test/java/tqs/api/WebPageSteps.java
@@ -1,39 +1,68 @@
-package tqs.api;
+ package tqs.api;
 
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import java.util.concurrent.TimeUnit;
 
-class WebPageSteps {
+public class WebPageSteps {
     
     private final WebDriver driver = new FirefoxDriver();
 
-    @Given("I'm on {string}")
-    void goToPage(String url){
-        driver.get(url);
+    @Before
+    public void setUp(){
+        driver.manage().timeouts().setScriptTimeout(100,TimeUnit.SECONDS);
     }
 
-    @When("I write on country input: {string}")
-    void writeCountry(String country){
-        
+     @After
+    public void tearDown() {
+      driver.quit();
     }
+ 
+     @Given("I'm on {string}")
+     public void goToPage(String url){
+         driver.get(url);
+     }
+ 
+     @When("I write on country input: {string}")
+     public void writeCountry(String country){
+         driver.findElement(By.cssSelector(".form-control")).click();
+         driver.findElement(By.cssSelector(".form-control")).sendKeys(country);
+     }
+ 
+     @When("select {int} days")
+     public void selectNDays(int nDays){
+         driver.findElement(By.cssSelector(".col-2")).click();
+         driver.findElement(By.cssSelector(".col-2")).sendKeys(""+nDays);
+     }
+ 
+     @When("click on search")
+     public void pressButton(){
+         driver.findElement(By.id("search-btn")).click();
+     }
+ 
+     @Then("I should see a table with data for {string} of the last {int} days")
+     public void result(String country, int nDays) throws InterruptedException{
+        TimeUnit.SECONDS.sleep(5);
+        WebElement countryTitle = driver.findElement(By.id("countryTitle"));
+        assertThat(countryTitle.getText()).isEqualTo(country);
+        for (int i = 0; i < nDays; i++){
+            List<WebElement> elements = driver.findElements(By.id("day"+i));
+            assert(elements.size() > 0);
+        }
+         
+     }
 
-    @When("select {int} days")
-    void selectNDays(int nDays){
-
-    }
-
-    @When("click on search")
-    void pressButton(){
-
-    }
-
-    @Then("I should see a table with data for <country> of the last <n> days")
-    void result(String country, int nDays){
-        
-    }
-
-}
+ }
--- HW1/api/src/test/resources/tqs/api/webpage.feature
+++ HW1/api/src/test/resources/tqs/api/webpage.feature
@@ -1,20 +1,20 @@
 Feature: Check Covid Statistics
 
-    Scenario: Check the Default View of a Country
-        Given I'm on "http://localhost:3000/"
-        When I write on country input: "portugal"
-        And click on search
-        Then I should see a table with data for "portugal" of the last 3 days 
+  Scenario: Check the Default View of a Country
+    Given I'm on "http://localhost:3000/"
+    When I write on country input: "Portugal"
+    And click on search
+    Then I should see a table with data for "Portugal" of the last 3 days 
 
 
-    Scenario Outline: Check the View of a Country for Data of the Last n-days
-        Given I'm on "http://localhost:3000/"
-        When I write on country input: <country>
-        And select <n> days
-        And click on search
-        Then I should see a table with data for <country> of the last <n> days
-    Examples:
-        | country   | n |
-        | portugal  | 5 |
-        | usa       | 2 |
-        | spain     | 6 |
+  Scenario Outline: Check the View of a Country for Data of the Last n-days
+    Given I'm on "http://localhost:3000/"
+    When I write on country input: "<country>"
+    And select <n> days
+    And click on search
+    Then I should see a table with data for "<country>" of the last <n> days
+  Examples:
+    | country   | n |
+    | Portugal  | 5 |
+    | USA       | 2 |
+    | Spain     | 6 |
